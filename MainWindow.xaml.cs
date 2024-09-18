796cd04c428c1b48f1e1959026bf2ac48f922316
jonathanMora2005 <jonathan.mora@uvic.cat>
2024-09-18 15:54:49 +0200

    Agregar archivos de proyecto.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ActivarCampCheckbox_Checked(object sender, RoutedEventArgs e)
+        {
+            CampExtraTextBox.Visibility = Visibility.Visible;
+        }
+
+        private void ActivarCampCheckbox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            CampExtraTextBox.Visibility = Visibility.Collapsed;
+        }
+
+    }
+}
